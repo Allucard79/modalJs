dce8c3c50ce161c77466ab9e239a70956e2b0b5c
Marcin Ratajczak <maratajczak@gmail.com>
2018-11-20 19:15:34 +0100

    Change html css js code

--- js/script.js
+++ js/script.js
@@ -1,35 +1,38 @@
 'use strict';
-var showModal = function(event) {
+var showModal = function (href) {
+  return function (event) {
+    console.log('show modal')
     event.preventDefault();
     var allModals = document.querySelectorAll('.modal');
     for (var i = 0; i < allModals.length; i++) {
-        allModals[i].classList.remove('show');
+      allModals[i].classList.remove('show');
     }
-    document.querySelector('#modal-one').classList.add('show');
+    document.querySelector(href).classList.add('show');
     document.querySelector('#modal-overlay').classList.add('show');
-  
-};
-
-var modalLinks = document.querySelectorAll('.show-modal');
-for (var i = 0; i < modalLinks.length; i++) {
-    modalLinks[i].addEventListener('click', showModal);
+  };
 }
 
-var hideModal = function(event) {
-    event.preventDefault();
-    document.querySelector('#modal-overlay').classList.remove('show');
+var modalLinks = document.querySelectorAll('.show-modal');
+console.log('modalLinks', modalLinks)
+for (var i = 0; i < modalLinks.length; i++) {
+  var href = modalLinks[i].getAttribute('href')
+  modalLinks[i].addEventListener('click', showModal(href));
+}
+
+var hideModal = function (event) {
+  event.preventDefault();
+  document.querySelector('#modal-overlay').classList.remove('show');
 };
 
 var closeButtons = document.querySelectorAll('.modal .close');
 for (var i = 0; i < closeButtons.length; i++) {
-    closeButtons[i].addEventListener('click', hideModal);
+  closeButtons[i].addEventListener('click', hideModal);
 }
 
 document.querySelector('#modal-overlay').addEventListener('click', hideModal);
-
 var modals = document.querySelectorAll('.modal');
 for (var i = 0; i < modals.length; i++) {
-    modals[i].addEventListener('click', function(event) {
-        event.stopPropagation();
-    });
+  modals[i].addEventListener('click', function (event) {
+    event.stopPropagation();
+  });
 }
