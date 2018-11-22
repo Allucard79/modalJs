61240983e2b52b8ffc901d3100d8499c3bff9fec
Marcin Ratajczak <maratajczak@gmail.com>
2018-11-22 21:13:22 +0100

    Change showModal in js code

--- js/script.js
+++ js/script.js
@@ -1,38 +1,37 @@
-'use strict';
-var showModal = function () {
-  return function (event) {
-    console.log('show modal')
-    event.preventDefault();
-    var allModals = document.querySelectorAll('.modal');
+var showModal = function(event){
+  event.preventDefault();
+  var allModals = document.querySelectorAll('.modal');
     for (var i = 0; i < allModals.length; i++) {
       allModals[i].classList.remove('show');
     }
-    //document.querySelector(href).classList.add('show');
-    console.log(this)
-    document.querySelector('#modal-overlay').classList.add('show');
-  };
-}
+  var href = this.getAttribute('href');
+  document.querySelector(href).classList.add('show');
+  document.querySelector('#modal-overlay').classList.add('show');
+};
 
 var modalLinks = document.querySelectorAll('.show-modal');
-console.log('modalLinks', modalLinks)
-for (var i = 0; i < modalLinks.length; i++) {
+
+for(var i = 0; i < modalLinks.length; i++){
   modalLinks[i].addEventListener('click', showModal);
 }
 
-var hideModal = function (event) {
+var hideModal = function(event){
   event.preventDefault();
   document.querySelector('#modal-overlay').classList.remove('show');
 };
 
 var closeButtons = document.querySelectorAll('.modal .close');
-for (var i = 0; i < closeButtons.length; i++) {
+
+for(var i = 0; i < closeButtons.length; i++){
   closeButtons[i].addEventListener('click', hideModal);
 }
 
 document.querySelector('#modal-overlay').addEventListener('click', hideModal);
+
 var modals = document.querySelectorAll('.modal');
-for (var i = 0; i < modals.length; i++) {
-  modals[i].addEventListener('click', function (event) {
+
+for(var i = 0; i < modals.length; i++){
+  modals[i].addEventListener('click', function(event){
     event.stopPropagation();
   });
 }
